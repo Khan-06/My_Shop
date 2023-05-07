51ff1868855f7934ba4d0cddc64491ce24336537
Khan-06 <m.abdulbasit.k.06@gmail.com>
2023-05-07 19:37:04 +0500

    Adding plugin flushBar and removing order when button pressed

--- lib/providers/cart.dart
+++ lib/providers/cart.dart
@@ -13,21 +13,23 @@ class CartItem {
 }
 
 class Cart with ChangeNotifier {
-   Map<String, CartItem> _items = {};
+  Map<String, CartItem> _items = {};
 
   Map<String, CartItem> get items {
     return {..._items};
   }
+
   int get itemCount {
-    return items == null ? 0 :items.length;
-}
-double get totalAmount {
+    return items == null ? 0 : items.length;
+  }
+
+  double get totalAmount {
     double total = 0.0;
     _items.forEach((key, cartItem) {
       total += cartItem.price * cartItem.quantity;
     });
     return total;
-}
+  }
 
   void addItem(String productID, String title, double price) {
     if (_items.containsKey(productID)) {
@@ -53,12 +55,33 @@ double get totalAmount {
     }
   }
 
-  void removeItem (String productId){
+  void removeItem(String productId) {
     _items.remove(productId);
     notifyListeners();
   }
 
-  void clear (){
+  void removeSingleItem(String productID) {
+    print('object');
+    if (!_items.containsKey(productID)) {
+      print('reached0');
+      return;
+    } else if (_items[productID]!.quantity > 1) {
+      _items.update(
+        productID,
+        (value) => CartItem(
+          title: value.title,
+          id: value.id,
+          price: value.price,
+          quantity: value.quantity - 1,
+        ),
+      );
+    } else {
+      _items.remove(productID);
+    }
+    notifyListeners();
+  }
+
+  void clear() {
     _items = {};
     notifyListeners();
   }
--- lib/widgets/product_item.dart
+++ lib/widgets/product_item.dart
@@ -1,13 +1,13 @@
 import 'package:flutter/material.dart';
-import 'package:provider/provider.dart';
 
+import 'package:provider/provider.dart';
+import 'package:another_flushbar/flushbar.dart';
 
 import '../providers/product.dart';
 import '../providers/cart.dart';
 import '../screens/product_details_screen.dart';
 
 class ProductItem extends StatelessWidget {
-
   @override
   Widget build(BuildContext context) {
     final product = Provider.of<Product>(context, listen: false);
@@ -17,21 +17,40 @@ class ProductItem extends StatelessWidget {
       borderRadius: BorderRadius.circular(10),
       child: GridTile(
         footer: GridTileBar(
-          leading: Consumer<Product> (builder: (context, value, child) => IconButton(
-            onPressed: () {
-              product.toggleFavorite();
-            },
-            icon: Icon(
-              product.isFavorite ? Icons.favorite : Icons.favorite_border,            //Favorite
-              color: Theme.of(context).colorScheme.secondary,
+          leading: Consumer<Product>(
+            builder: (context, value, child) => IconButton(
+              onPressed: () {
+                product.toggleFavorite();
+              },
+              icon: Icon(
+                product.isFavorite
+                    ? Icons.favorite
+                    : Icons.favorite_border, //Favorite
+                color: Theme.of(context).colorScheme.secondary,
+              ),
             ),
-          ),),
+          ),
           backgroundColor: Colors.black54,
           title: Text(product.title, textAlign: TextAlign.center),
           trailing: IconButton(
             color: Theme.of(context).colorScheme.secondary,
-            onPressed: () {cart.addItem(product.id, product.title, product.price);},
-            icon: const Icon(Icons.shopping_cart),                                    //Shopping cart
+            onPressed: () {
+              cart.addItem(product.id, product.title, product.price);
+              Flushbar(
+                title: 'Product Added',
+                message: '${product.title} added to your cart',
+                duration: const Duration(seconds: 2),
+                margin: const EdgeInsets.all(5),
+                borderRadius: BorderRadius.circular(20),
+                mainButton: ElevatedButton(
+                  onPressed: () {
+                    cart.removeSingleItem(product.id);
+                  },
+                  child: const Text('Undo'),
+                ),
+              ).show(context);
+            },
+            icon: const Icon(Icons.shopping_cart), //Shopping cart
           ),
         ),
         child: GestureDetector(
