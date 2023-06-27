8a427f7152fe99ae5649eba9f05f249ada78ee9f
Khan-06 <m.abdulbasit.k.06@gmail.com>
2023-06-27 20:53:28 +0500

    Module 10 complete!

--- lib/providers/orders.dart
+++ lib/providers/orders.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
 import 'dart:convert';
+
 import './cart.dart';
 
 class OrderItem {
@@ -11,73 +12,77 @@ class OrderItem {
 
   OrderItem(
       {required this.id,
-      required this.amount,
-      required this.products,
-      required this.dateTime});
+        required this.amount,
+        required this.products,
+        required this.dateTime});
 }
 
 class Orders with ChangeNotifier {
-   List<OrderItem> _orders = [];
+  List<OrderItem> _orders = [];
 
   List<OrderItem> get orders {
     return [..._orders];
   }
-   Future<void> fetchAndSetOrders() async {
-     final url = Uri.https('flutter-update.firebaseio.com', '/orders.json');
-     final response = await http.get(url);
-     final List<OrderItem> loadedOrders = [];
-     final extractedData = json.decode(response.body) as Map<String, dynamic>;
-     if (extractedData == null) {
-       return;
-     }
-     extractedData.forEach((orderId, orderData) {
-       loadedOrders.add(
-         OrderItem(
-           id: orderId,
-           amount: orderData['amount'],
-           dateTime: DateTime.parse(orderData['dateTime']),
-           products: (orderData['products'] as List<dynamic>)
-               .map(
-                 (item) => CartItem(
-               id: item['id'],
-               price: item['price'],
-               quantity: item['quantity'],
-               title: item['title'],
-             ),
-           ).toList(),
-         ),
-       );
-     });
-     _orders = loadedOrders.reversed.toList();
-     notifyListeners();
-   }
 
+  Future<void> fetchAndSetOrders() async {
+    var url = Uri.https('my-shop-a4071-default-rtdb.firebaseio.com', '/orders.json');
+    final response = await http.get(url);
+
+    final extractedData = json.decode(response.body) as Map<String, dynamic>?;
+    final List<OrderItem> loadedData = [];
+
+    if (extractedData == null) {
+      _orders = [];
+      notifyListeners();
+      return;
+    }
+
+    extractedData.forEach((orderID, orderData) {
+
+      loadedData.add(OrderItem(
+        id: orderID,
+        amount: orderData['amount'],
+        products: (orderData['products'] as List<dynamic>)
+            .map(
+              (item) => CartItem(
+            title: item['title'],
+            id: item['id'],
+            price: item['price'],
+            quantity: item['quantity'],
+          ),
+        )
+            .toList(),
+        dateTime: DateTime.parse(orderData['dateTime']),
+      ));
+    });
+
+    _orders = loadedData.reversed.toList();
+    notifyListeners();
+  }
   Future<void> addOrder(List<CartItem> cartProducts, double total) async {
-    var timeStamp = DateTime.now();
     var url =
-        Uri.https('my-shop-a4071-default-rtdb.firebaseio.com', '/orders.json');
+    Uri.https('my-shop-a4071-default-rtdb.firebaseio.com', '/orders.json');
+    final timeStamp = DateTime.now();
     final response = await http.post(url,
         body: json.encode({
           'amount': total,
           'dateTime': timeStamp.toIso8601String(),
           'products': cartProducts
               .map((cp) => {
-                    'title': cp.title,
-                    'price': cp.price,
-                    'quantity': cp.quantity,
-                    'id': cp.id
-                  })
+            'title': cp.title,
+            'id': cp.id,
+            'price': cp.price,
+            'quantity': cp.quantity
+          })
               .toList()
         }));
-    print(response.body);
     _orders.insert(
       0,
       OrderItem(
-        id: json.decode(response.body)['name'],
-        amount: total,
-        products: cartProducts,
-        dateTime: timeStamp,
-      ),
+          id: json.decode(response.body)['name'],
+          amount: total,
+          products: cartProducts,
+          dateTime: timeStamp),
     );
     notifyListeners();
   }
