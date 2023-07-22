2a5b4a329c34bf7996a600b7a5bd18afb08a6892
Khan-06 <m.abdulbasit.k.06@gmail.com>
2023-07-22 18:35:33 +0500

    Fixing some errors!

--- lib/screens/user_product_screen.dart
+++ lib/screens/user_product_screen.dart
@@ -17,29 +17,38 @@ class UserProductScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    final productsData = Provider.of<Products>(context);
+
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('Your Products'),
-        actions: [
-          IconButton(
-            onPressed: () {
-              Navigator.pushNamed(context, EditProductsScreen.routeName);
-            },
-            icon: const Icon(Icons.add),
-          ),
-        ],
-      ),
-      drawer: const AppDrawer(),
-      body: RefreshIndicator(
-        onRefresh: () => _refreshPage(context),
-        child: ListView.builder(
-            itemBuilder: (_, index) => UserProductItem(
-                id: productsData.items[index].id,
-                title: productsData.items[index].title,
-                imageUrl: productsData.items[index].imageUrl),
-            itemCount: productsData.items.length),
-      ),
-    );
+        appBar: AppBar(
+          title: const Text('Your Products'),
+          actions: [
+            IconButton(
+              onPressed: () {
+                Navigator.pushNamed(context, EditProductsScreen.routeName);
+              },
+              icon: const Icon(Icons.add),
+            ),
+          ],
+        ),
+        drawer: const AppDrawer(),
+        body: FutureBuilder(
+            future: _refreshPage(context),
+            builder: (ctx, snapShot) => snapShot.connectionState ==
+                    ConnectionState.waiting
+                ? const Center(child: CircularProgressIndicator())
+                : RefreshIndicator(
+                    onRefresh: () => _refreshPage(context),
+                    child: Consumer<Products>(
+                      builder: (ctx, productsData, _) => Padding(
+                        padding: const EdgeInsets.all(9),
+                        child: ListView.builder(
+                            itemBuilder: (_, index) => UserProductItem(
+                                id: productsData.items[index].id,
+                                title: productsData.items[index].title,
+                                imageUrl: productsData.items[index].imageUrl),
+                            itemCount: productsData.items.length),
+                      ),
+                    ),
+                  )));
   }
 }
