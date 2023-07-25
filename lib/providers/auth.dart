0224e60c7c34e3c778dd7ebdcaac3a68e91abbee
Khan-06 <m.abdulbasit.k.06@gmail.com>
2023-07-26 01:44:50 +0500

    working on saving data in device!

--- lib/providers/auth.dart
+++ lib/providers/auth.dart
@@ -1,23 +1,27 @@
 import 'package:flutter/material.dart';
+
 import 'dart:convert';
 import 'dart:async';
 
 import 'package:http/http.dart' as http;
+import 'package:shared_preferences/shared_preferences.dart';
 
 import '../models/http_exception.dart';
 
 class Auth with ChangeNotifier {
-    String? _token;
-  late  DateTime? _expiryDate = null; //Check for errors later
-    String? _userId;
-    Timer? _authTimer;
+  String? _token;
+  late DateTime? _expiryDate = null; //Check for errors later
+  String? _userId;
+  Timer? _authTimer;
 
   bool get isAuth {
-   return token != null;
+    return token != null;
   }
 
   String? get token {
-    if(_expiryDate != null && _expiryDate!.isAfter(DateTime.now()) && _token != null){
+    if (_expiryDate != null &&
+        _expiryDate!.isAfter(DateTime.now()) &&
+        _token != null) {
       return _token;
     }
     return null;
@@ -27,10 +31,11 @@ class Auth with ChangeNotifier {
     return _userId!;
   }
 
-  Future<void> _authentication (String email, String password, String urlSegment) async {
+  Future<void> _authentication(
+      String email, String password, String urlSegment) async {
     final url = Uri.parse(
         'https://identitytoolkit.googleapis.com/v1/accounts:$urlSegment?key=AIzaSyAh2sctSnLg_nyUPrrGnpjyzQa1KlIKheU');
-    try{
+    try {
       final response = await http.post(
         url,
         body: json.encode(
@@ -39,15 +44,23 @@ class Auth with ChangeNotifier {
       );
       print(json.decode(response.body));
       final responseData = json.decode(response.body);
-      if(responseData == null){
+      if (responseData == null) {
         throw HttpException(responseData['error']['message']);
       }
       _token = responseData['idToken'];
       _userId = responseData['localId'];
-      _expiryDate = DateTime.now().add(Duration(seconds: int.parse(responseData['expiresIn']),),);
+      _expiryDate = DateTime.now().add(
+        Duration(
+          seconds: int.parse(responseData['expiresIn']),
+        ),
+      );
       autoLogOut();
       notifyListeners();
-    }catch(error){
+      final prefrences = await SharedPreferences.getInstance();
+      final userData = json.encode(
+          {'token': _token, 'userId': _userId, 'expiryDate': _expiryDate?.toIso8601String()});
+      prefrences.setString('userData', userData);
+    } catch (error) {
       rethrow;
     }
   }
@@ -55,26 +68,30 @@ class Auth with ChangeNotifier {
   Future<void> signUp(String email, String password) async {
     return _authentication(email, password, 'signUp');
   }
-  Future<void> logIn (String email, String password) async {
-   return _authentication(email, password, 'signInWithPassword');
+
+  Future<void> logIn(String email, String password) async {
+    return _authentication(email, password, 'signInWithPassword');
   }
 
-  void logout(){
+  void logout() {
     _token = null;
     _userId = '';
     _expiryDate = null;
-    if(_authTimer != null){
+    if (_authTimer != null) {
       _authTimer!.cancel();
       _authTimer = null;
     }
     notifyListeners();
   }
 
-  void autoLogOut (){
-    if(_authTimer != null){
+  void autoLogOut() {
+    if (_authTimer != null) {
       _authTimer!.cancel();
     }
     final timeToExpiry = _expiryDate!.difference(DateTime.now()).inSeconds;
-    _authTimer = Timer(Duration(seconds: timeToExpiry), logout,);
+    _authTimer = Timer(
+      Duration(seconds: timeToExpiry),
+      logout,
+    );
   }
 }
