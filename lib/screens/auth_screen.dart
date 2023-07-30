16fcacbb9d0d53de85afb87a6a8c8139be0a2165
Khan-06 <m.abdulbasit.k.06@gmail.com>
2023-07-30 23:24:27 +0500

    Using the AnimatedContainer instead of AnimatedBuilder, which controls everything itself!

--- lib/screens/auth_screen.dart
+++ lib/screens/auth_screen.dart
@@ -207,17 +207,16 @@ class _AuthCardState extends State<AuthCard>
         borderRadius: BorderRadius.circular(10.0),
       ),
       elevation: 8.0,
-      child: AnimatedBuilder(
-        animation: _heightAnimation,
-        builder: (context, ch) => Container(
-            //height: _authMode == AuthMode.signUp ? 320 : 260,
-            height: _heightAnimation.value.height,
+      child: AnimatedContainer(
+            height: _authMode == AuthMode.signUp ? 320 : 260,
+            //height: _heightAnimation.value.height,
             constraints:
-                BoxConstraints(minHeight: _heightAnimation.value.height),
+                BoxConstraints(minHeight: _authMode == AuthMode.signUp ? 320 : 260),
             width: deviceSize.width * 0.75,
             padding: const EdgeInsets.all(16.0),
-            child: ch),
-        child: Form(
+            curve: Curves.easeIn,
+            duration: const Duration(milliseconds: 300),
+            child: Form(
           key: _formKey,
           child: SingleChildScrollView(
             child: Column(
@@ -304,8 +303,3 @@ class _AuthCardState extends State<AuthCard>
     );
   }
 }
-
-//
-// padding: EdgeInsets.symmetric(horizontal: 30.0, vertical: 4),
-// materialTapTargetSize: MaterialTapTargetSize.shrinkWrap,
-// textColor: Theme.of(context).primaryColor,
