596b5c281746fcf35d38366d9600f06bbc6e92ca
Khan-06 <m.abdulbasit.k.06@gmail.com>
2023-08-03 20:05:45 +0500

    Adding image in pubspec.yaml!

--- lib/widgets/product_item.dart
+++ lib/widgets/product_item.dart
@@ -61,7 +61,7 @@ class ProductItem extends StatelessWidget {
             Navigator.pushNamed(context, ProductDetails.routeName,
                 arguments: product.id);
           },
-          child: Image.network(product.imageUrl, fit: BoxFit.cover),
+          child: FadeInImage(placeholder: AssetImage('assets/images/product-placeholder.png'), image: NetworkImage(product.imageUrl)),
         ),
       ),
     );
